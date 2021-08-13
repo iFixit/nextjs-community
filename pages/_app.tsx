86b3a68fc874a441fbfa1699d619d9b8dc8cb651
Bhumin Ben Son <bhumin0804@gmail.com>
2021-08-13 14:59:45 -0700

    delete old chakra files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,10 @@ import '../styles/global.css'
 import React from 'react'
 import '@core-ds/primitives/core-primitives.css'
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../theme/index';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
+        <ChakraProvider>
             <Component {...pageProps} />
         </ChakraProvider>
     );
