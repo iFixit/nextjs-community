1f1a08b718247108fb86584c472dad5598295305
Bhumin Ben Son <bhumin0804@gmail.com>
2021-08-16 16:43:01 -0700

    update comment in lang

--- lib/lang.tsx
+++ lib/lang.tsx
@@ -1,6 +1,6 @@
 // A placeholder function for now, we'll have to implement this somehow.
 // Equivalent to UserLanguagePreference::getLangid($user) 
-
+// TODO: IMPLEMENT THIS
 export function isEnglish() {
     return true;
 }
