947eff70c3d24f4780edf572ea62b4529bb01314
Bhumin Ben Son <bhumin0804@gmail.com>
2021-08-17 15:54:54 -0700

    mock ifixit header and footer

--- components/ifixit/footer.tsx
+++ components/ifixit/footer.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import styles from '../../styles/ifixit/footer.module.css'
 
 export default function Footer() {
     return (
         <footer className={styles.container}>
-            Footer
+            <Image src='/images/footer.png' alt='' layout='fill' objectFit='cover' />
         </footer>
     )
 }
--- components/ifixit/header.tsx
+++ components/ifixit/header.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import styles from '../../styles/ifixit/header.module.css'
 
 export default function Header() {
     return (
         <header className={styles.container}>
-            iFixit
+            <Image src='/images/header.png' alt='' layout='fill' objectFit='contain' />
         </header>
     )
 }
