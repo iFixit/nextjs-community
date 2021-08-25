f294d7d303b964f63eeaf2703760b5f89a72218b
Bhumin Ben Son <bhumin0804@gmail.com>
2021-08-25 15:22:16 -0700

    add footer data files

---
+++ components/ifixit/footer/links.tsx
@@ -0,0 +1,22 @@
+import links from '../../../lib/links';
+
+const linkMappings = [
+   [
+      { label: 'About Us', url: links.ABOUT_US },
+      { label: 'Customer Support', url: links.HELP },
+      { label: 'Careers', url: links.CAREERS },
+      { label: 'Feedback', url: links.FEEDBACK },
+      { label: 'API', url: links.API },
+      { label: 'Newsletter', url: links.NEWSLETTER },
+   ],
+   [
+      { label: 'Press', url: links.PRESS },
+      { label: 'News', url: links.NEWS },
+      { label: 'Contribute', url: links.CONTRIBUTE },
+      { label: 'Pro Wholesale', url: links.PRO },
+      { label: 'Retail Locator', url: links.RETAIL_LOCATOR },
+      { label: 'For Manufacturers', url: links.MANUFACTURERS },
+   ],
+];
+
+export default linkMappings;
