b2f0b6c4a2a6329ab310d225c49c3430f336a591
Bhumin Ben Son <bhumin0804@gmail.com>
2021-09-01 09:52:40 -0700

    add some isMod code

--- components/community/navigation.tsx
+++ components/community/navigation.tsx
@@ -16,112 +16,120 @@ import React, { useState } from 'react';
 import LoginModal from '../login/modal';
 import links from '../../lib/links';
 
-const isOnIfixit = true;
-const isMod = true;
-const patrolEnabled = true;
-
 interface TabData {
    url: string;
    label: string;
    showTab: boolean;
 }
 
-const tabData: Array<TabData> = [
-   {
-      url: links.COMMUNITY,
-      label: 'Community',
-      showTab: isOnIfixit,
-   },
-   {
-      url: links.LEADERBOARD,
-      label: 'Leaderboard',
-      showTab: true,
-   },
-   {
-      url: links.ALL_TEAMS,
-      label: 'Teams',
-      showTab: true,
-   },
-   {
-      url: links.BUSINESS,
-      label: 'Businesses',
-      showTab: isOnIfixit,
-   },
-   {
-      url: links.CONTRIBUTE,
-      label: 'Contribute',
-      showTab: true,
-   },
-   {
-      url: links.TRANSLATE,
-      label: 'Translate',
-      showTab: true,
-   },
-   {
-      url: links.PATROL,
-      label: 'Patrol',
-      showTab: isMod && patrolEnabled,
-   },
-   {
-      url: links.MODERATION,
-      label: 'Moderation',
-      showTab: isMod,
-   },
-];
-
-const tabs = tabData.filter((tab: TabData) => tab.showTab);
-
-function ChakraTabs(title: string) {
-   const router = useRouter();
-
-   return tabs.map((tab: TabData, index: number) => {
-      const url = tab.url;
-      const label = tab.label;
-      return (
-         <Tab
-            className={label === title ? 'active' : ''}
-            key={index}
-            minWidth="100px"
-            onClick={() => router.push(url)}
-            sx={{
-               '&.active': {
-                  color: 'var(--color-blue)',
-                  borderColor: 'var(--color-blue)',
-               },
-               '&:hover:not(.active)': {
-                  color: 'var(--color-gray-6)',
-                  borderColor: 'var(--color-gray-4)',
-               },
-            }}
-         >
-            {label}
-         </Tab>
-      );
-   });
-}
-
-function listTabPanelComponents() {
-   return tabs.map(tabInfo => {
-      return <TabPanel key={tabInfo.label}></TabPanel>;
-   });
-}
-
-function getActiveTabIndex(pageTitle: string) {
-   let index = 0;
-   while (index < tabs.length) {
-      const tab = tabs[index];
-      if (tab.label === pageTitle) {
-         break;
-      }
-      index++;
-   }
-   return index;
-}
-
 export default function NavigationDisplay({ title }: { title: string }) {
    const { isOpen, onOpen, onClose } = useDisclosure();
    const [user, setUser] = useState();
    const router = useRouter();
+   const isMod = true;
+   const isOnIfixit = true;
+   const patrolEnabled = true;
+   const visibleTabs = getTabs();
+
+   // async function isModerator() {
+   //    if (user and user.userid) {
+   //       await fetch('https://bson.cominor.com/api/2.0/community/isMod?'+ user.userid);
+   //       .then(response => response.json())
+   //       .then(data => console.log(data));
+   //    }
+   // }
+
+   function getTabs(): Array<TabData> {
+      return [
+         {
+            url: links.COMMUNITY,
+            label: 'Community',
+            showTab: isOnIfixit,
+         },
+         {
+            url: links.LEADERBOARD,
+            label: 'Leaderboard',
+            showTab: true,
+         },
+         {
+            url: links.ALL_TEAMS,
+            label: 'Teams',
+            showTab: true,
+         },
+         {
+            url: links.BUSINESS,
+            label: 'Businesses',
+            showTab: isOnIfixit,
+         },
+         {
+            url: links.CONTRIBUTE,
+            label: 'Contribute',
+            showTab: true,
+         },
+         {
+            url: links.TRANSLATE,
+            label: 'Translate',
+            showTab: true,
+         },
+         {
+            url: links.PATROL,
+            label: 'Patrol',
+            showTab: isMod && patrolEnabled,
+         },
+         {
+            url: links.MODERATION,
+            label: 'Moderation',
+            showTab: isMod,
+         },
+      ].filter((tab: TabData) => tab.showTab);
+   }
+
+   function ChakraTabs(title: string) {
+      const router = useRouter();
+
+      return visibleTabs.map((tab: TabData, index: number) => {
+         const url = tab.url;
+         const label = tab.label;
+         return (
+            <Tab
+               className={label === title ? 'active' : ''}
+               key={index}
+               minWidth="100px"
+               onClick={() => router.push(url)}
+               sx={{
+                  '&.active': {
+                     color: 'var(--color-blue)',
+                     borderColor: 'var(--color-blue)',
+                  },
+                  '&:hover:not(.active)': {
+                     color: 'var(--color-gray-6)',
+                     borderColor: 'var(--color-gray-4)',
+                  },
+               }}
+            >
+               {label}
+            </Tab>
+         );
+      });
+   }
+
+   function listTabPanelComponents() {
+      return visibleTabs.map(tabInfo => {
+         return <TabPanel key={tabInfo.label}></TabPanel>;
+      });
+   }
+
+   function getActiveTabIndex(pageTitle: string) {
+      let index = 0;
+      while (index < visibleTabs.length) {
+         const tab = visibleTabs[index];
+         if (tab.label === pageTitle) {
+            break;
+         }
+         index++;
+      }
+      return index;
+   }
 
    return (
       <React.Fragment>
@@ -171,6 +179,7 @@ export default function NavigationDisplay({ title }: { title: string }) {
                <TabList>{ChakraTabs(title)}</TabList>
                <TabPanels display="none">{listTabPanelComponents()}</TabPanels>
             </Tabs>
+            {/* <Button onClick={isModerator}>asdf</Button> */}
          </Box>
       </React.Fragment>
    );
