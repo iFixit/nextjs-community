e06b4c483ec48b2fa6403474e13a250885d7c812
Bhumin Ben Son <bhumin0804@gmail.com>
2021-09-03 10:55:37 -0700

    use getImage for partner logos

---
+++ components/ifixit/footer/partners.tsx
@@ -0,0 +1,42 @@
+import { getImage } from '../../../lib/images';
+
+const partners = [
+   {
+      name: 'VISA',
+      logo: getImage('footer/visa.png'),
+   },
+   {
+      name: 'MasterCard',
+      logo: getImage('footer/mc.png'),
+   },
+   {
+      name: 'AMEX',
+      logo: getImage('footer/amex.png'),
+   },
+   {
+      name: 'PayPal',
+      logo: getImage('footer/paypal.png'),
+   },
+   {
+      name: 'SoFort',
+      logo: getImage('footer/sofort.png'),
+   },
+   {
+      name: 'DHL',
+      logo: getImage('footer/dhl.png'),
+   },
+   {
+      name: 'FedEx',
+      logo: getImage('footer/fedex.png'),
+   },
+   {
+      name: 'UPS',
+      logo: getImage('footer/ups.png'),
+   },
+   {
+      name: 'Hermes',
+      logo: getImage('footer/hermes.png'),
+   },
+];
+
+export default partners;
