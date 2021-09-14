5a2027375780fa9fa35040731fd11fc64ead9e3f
Bhumin Ben Son <bhumin0804@gmail.com>
2021-09-14 14:17:38 -0700

    edit desktop and mobile views

---
+++ components/ifixit/footer/desktopView.tsx
@@ -0,0 +1,87 @@
+import { Heading, Img, SimpleGrid, Box, Stack } from '@chakra-ui/react';
+import { FooterMenuList, FooterMenuItem, FooterPartnerLink } from '@ifixit/react-components';
+import React from 'react';
+import { getFooterCategories, listCategory } from './links';
+import Menus from './menus';
+import NewsletterSignup from './newsletter';
+import partners from './partners';
+
+export function Links() {
+   const categories = getFooterCategories();
+
+   return (
+      <Stack
+         direction={{ base: 'column', xl: 'row' }}
+         mt="48px"
+         spacing={{ base: '32px', xl: '14px' }}
+      >
+         <SimpleGrid
+            columns={{
+               base: 1,
+               sm: 3,
+            }}
+            spacing="52px"
+            flexGrow={{ base: 'unset', xl: 1 }}
+         >
+            {categories.map(category => (
+               <FooterMenuList key={category.title} border="none" pb="0">
+                  <FooterMenuItem mb="10px" display={{ base: 'none', sm: 'inherit' }}>
+                     <Heading fontSize="14px" color="trueGray.100">
+                        {category.title}
+                     </Heading>
+                  </FooterMenuItem>
+                  {listCategory(category)}
+               </FooterMenuList>
+            ))}
+         </SimpleGrid>
+         <Partners />
+      </Stack>
+   );
+}
+
+function Partners() {
+   return (
+      <SimpleGrid
+         columns={3}
+         spacing="4"
+         display="grid"
+         w={{ base: '100%', xl: '350px' }}
+         pb={{ base: '0', xl: '52px' }}
+      >
+         {partners.map(partner => {
+            return (
+               <FooterPartnerLink key={partner.name} pointerEvents="none" p="12px">
+                  <Img
+                     h="full"
+                     mx="auto"
+                     objectFit="contain"
+                     src={partner.logo}
+                     alt={partner.name}
+                  />
+               </FooterPartnerLink>
+            );
+         })}
+      </SimpleGrid>
+   );
+}
+
+export default function View() {
+   return (
+      <Box
+         display={{ base: 'none', sm: 'unset' }}
+         margin={{ base: '0 5%', xl: '0 auto' }}
+         width={{ base: '90%', xl: '1000px' }}
+      >
+         <Links />
+         <Stack
+            direction={{ base: 'column', lg: 'row' }}
+            justify="space-between"
+            align={{ base: 'unset', lg: 'flex-end' }}
+            spacing="18px"
+         >
+            <NewsletterSignup />
+            <Menus />
+         </Stack>
+      </Box>
+   );
+}
---
+++ components/ifixit/footer/mobileView.tsx
@@ -0,0 +1,50 @@
+import {
+   Accordion,
+   AccordionButton,
+   AccordionIcon,
+   AccordionItem,
+   AccordionPanel,
+} from '@chakra-ui/accordion';
+import { Box } from '@chakra-ui/layout';
+import React from 'react';
+import { getFooterCategories, listCategory } from './links';
+import Menus from './menus';
+import NewsletterSignup from './newsletter';
+
+export default function MobileView() {
+   const categories = getFooterCategories();
+
+   return (
+      <React.Fragment>
+         <Box display={{ base: 'unset', sm: 'none' }}>
+            <Accordion allowToggle borderColor="trueGray.700" mb="18px">
+               {categories.map(category => {
+                  return (
+                     <AccordionItem key={category.title}>
+                        <AccordionButton height="48px">
+                           <Box flex="1" textAlign="left" fontWeight="bold">
+                              {category.title}
+                           </Box>
+                           <AccordionIcon />
+                        </AccordionButton>
+                        <AccordionPanel>{listCategory(category)}</AccordionPanel>
+                     </AccordionItem>
+                  );
+               })}
+               <AccordionItem>
+                  <AccordionButton height="48px">
+                     <Box flex="1" textAlign="left" fontWeight="bold">
+                        Newsletter
+                     </Box>
+                     <AccordionIcon />
+                  </AccordionButton>
+                  <AccordionPanel>
+                     <NewsletterSignup />
+                  </AccordionPanel>
+               </AccordionItem>
+            </Accordion>
+            <Menus />
+         </Box>
+      </React.Fragment>
+   );
+}
