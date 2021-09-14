d6f20116522f397a8bbd12e10f38c6b312708fcb
Bhumin Ben Son <bhumin0804@gmail.com>
2021-09-14 14:17:27 -0700

    break up footer components

--- components/ifixit/footer/footer.tsx
+++ components/ifixit/footer/footer.tsx
@@ -1,120 +1,51 @@
-import { SimpleGrid, Divider, Stack, Menu, MenuList, HStack } from '@chakra-ui/react';
-import {
-   FlagIcon,
-   Footer,
-   FooterLink,
-   Globe,
-   StoreCurrency,
-   StoreFlagBackdrop,
-   StoreMenuButton,
-   StoreMenuItem,
-   StoreName,
-} from '@ifixit/react-components';
+import { Stack, HStack, Divider } from '@chakra-ui/react';
+import { Footer, FooterLink } from '@ifixit/react-components';
 import React from 'react';
-import stores from './stores';
 import links from '../../../lib/links';
-import DesktopLinks from './desktopLinks';
-import MobileLinks from './mobileLinks';
-import { CountryCode } from '@ifixit/react-components';
+import DesktopView from './desktopView';
+import MobileView from './mobileView';
 
 export default function FooterComponent() {
    return (
       <Footer bgColor="var(--color-black)">
-         <DesktopLinks />
-         <MobileLinks />
-         <Divider borderColor="trueGray.700" />
-         <FooterBottom />
+         <DesktopView />
+         <MobileView />
+         <Divider display={{ base: 'none', sm: 'unset' }} mt="14px" borderColor="trueGray.700" />
+         <Legal />
       </Footer>
    );
 }
 
-function FooterBottom() {
+function Legal() {
    return (
-      <SimpleGrid
-         columns={{
-            base: 1,
-            lg: 2,
-         }}
-         py="6"
-         spacing={{
-            base: 10,
-         }}
+      <Stack
          w={{ base: '90%', xl: '1000px' }}
          margin="0 auto"
+         py="24px"
+         align="center"
+         justify={{
+            base: 'center',
+         }}
+         direction={{
+            base: 'column',
+            sm: 'row',
+         }}
+         spacing={4}
       >
-         <Stack
-            direction={{
-               base: 'row',
-            }}
-            spacing={{
-               base: 2,
-               sm: 12,
-            }}
-            justify={{
-               base: 'space-between',
-               sm: 'center',
-               lg: 'flex-start',
-            }}
-         >
-            <Menu>
-               <StoreMenuButton
-                  icon={
-                     <StoreFlagBackdrop>
-                        <FlagIcon code={CountryCode.US} />
-                     </StoreFlagBackdrop>
-                  }
-               >
-                  Region
-               </StoreMenuButton>
-               <MenuList>
-                  {stores.map(store => {
-                     return (
-                        <StoreMenuItem key={store.storeCode} as="a" href={store.url}>
-                           <FlagIcon code={store.storeCode.toUpperCase() as CountryCode} />
-                           <StoreName>{store.name}</StoreName>
-                           <StoreCurrency>{store.currency}</StoreCurrency>
-                        </StoreMenuItem>
-                     );
-                  })}
-               </MenuList>
-            </Menu>
-            <FooterLink href={links.TRANSLATE} icon={Globe}>
-               Help translate
+         <FooterLink color="trueGray.400" href={links.LICENSING}>
+            © iFixit 2021
+         </FooterLink>
+         <FooterLink textAlign="center" color="trueGray.400" href={links.LICENSING}>
+            Licensed under creative commons
+         </FooterLink>
+         <HStack spacing={4}>
+            <FooterLink color="trueGray.400" href={links.PRIVACY}>
+               Privacy
             </FooterLink>
-         </Stack>
-         <Stack
-            align="center"
-            justify={{
-               base: 'flex-start',
-               sm: 'center',
-               lg: 'flex-end',
-            }}
-            direction={{
-               base: 'column',
-               sm: 'row',
-            }}
-            spacing={{
-               base: 2,
-               sm: 4,
-            }}
-         >
-            <FooterLink color="trueGray.400" href={links.LICENSING}>
-               Licensed under creative commons
+            <FooterLink color="trueGray.400" href={links.TERMS_OF_USE}>
+               Legal
             </FooterLink>
-            <HStack
-               spacing={{
-                  base: 2,
-                  sm: 4,
-               }}
-            >
-               <FooterLink color="trueGray.400" href={links.PRIVACY}>
-                  Privacy
-               </FooterLink>
-               <FooterLink color="trueGray.400" href={links.TERMS_OF_USE}>
-                  Legal
-               </FooterLink>
-            </HStack>
-         </Stack>
-      </SimpleGrid>
+         </HStack>
+      </Stack>
    );
 }
---
+++ components/ifixit/footer/menus.tsx
@@ -0,0 +1,80 @@
+import { HStack, Stack, Text } from '@chakra-ui/layout';
+import { Menu, MenuList } from '@chakra-ui/menu';
+import {
+   CountryCode,
+   FlagIcon,
+   FooterLink,
+   Globe,
+   StoreCurrency,
+   StoreFlagBackdrop,
+   StoreMenuButton,
+   StoreMenuItem,
+   StoreName,
+} from '@ifixit/react-components';
+import React from 'react';
+import links from '../../../lib/links';
+import stores from './stores';
+
+export default function Menus() {
+   return (
+      <Stack
+         direction={{ base: 'column', xs: 'row' }}
+         spacing={{ base: 3, xs: 1, sm: 10 }}
+         align={{ base: 'center', xs: 'baseline' }}
+         justify={{ base: 'space-between', sm: 'center' }}
+         px="18px"
+      >
+         <Menu>
+            <StoreMenuButton
+               icon={
+                  <StoreFlagBackdrop>
+                     <FlagIcon code={CountryCode.US} />
+                  </StoreFlagBackdrop>
+               }
+               color="trueGray.400"
+            >
+               Region
+            </StoreMenuButton>
+            <MenuList>
+               {stores.map(store => {
+                  return (
+                     <StoreMenuItem key={store.storeCode} as="a" href={store.url}>
+                        <FlagIcon code={store.storeCode.toUpperCase() as CountryCode} />
+                        <StoreName>{store.name}</StoreName>
+                        <StoreCurrency>{store.currency}</StoreCurrency>
+                     </StoreMenuItem>
+                  );
+               })}
+            </MenuList>
+         </Menu>
+         <Menu>
+            <StoreMenuButton
+               icon={
+                  <StoreFlagBackdrop py="5px">
+                     <Text fontWeight="bold" fontSize="12px">
+                        EN
+                     </Text>
+                  </StoreFlagBackdrop>
+               }
+               color="trueGray.400"
+            >
+               Language
+            </StoreMenuButton>
+            <MenuList>
+               {stores.map(store => {
+                  return (
+                     <StoreMenuItem key={store.storeCode} as="a" href={store.url}>
+                        <FlagIcon code={store.storeCode.toUpperCase() as CountryCode} />
+                        <StoreName>{store.name}</StoreName>
+                        <StoreCurrency>{store.currency}</StoreCurrency>
+                     </StoreMenuItem>
+                  );
+               })}
+            </MenuList>
+         </Menu>
+         <FooterLink color="trueGray.400" href={links.TRANSLATE} icon={Globe}>
+            Help translate
+         </FooterLink>
+      </Stack>
+   );
+}
